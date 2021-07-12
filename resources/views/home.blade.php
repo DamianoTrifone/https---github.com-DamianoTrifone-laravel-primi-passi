7a8adc46c615547d85600f2dcfbd0efd5847b1f2
DamianoTrifone <trifone.damiano@gmail.com>
2021-07-12 16:58:28 +0200

    Definitivo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gilda+Display&display=swap" rel="stylesheet">
     <title>Hello World</title>
 </head>
 <body>
     <h1>Hello World!</h1>
-    <h1>Ciao {{$nome}} {{$cognome}}</h1>
+    <h1>Ciao, {{$nome}} {{$cognome}}. Questo è il tuo primo esercizio svolto con Laravel.</h1>
+    <style>
+        body{
+            background-image: url(../laravel-logo.jpg);
+            background-position: center;
+            color: #FF2C1F;
+            font-family: 'Gilda Display', serif;
+            text-align: center;
+            margin-top: 23%;
+        }
+    </style>
 </body>
 </html>
